68f3b9967ef7b916b0dee6c441fb598caf2cd803
Similoluwa Oyebajo <similoluwaoyebajo@gmail.com>
2022-07-12 13:44:40 -0700

    I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -2,47 +2,17 @@
 #include <stdarg.h>
 
 /**
- * check_format - checks if there is a valid format specifier
- * @format: possible valid format specifier
- * Return: pointer to valid function or NULL
+ * _printf - function that produces output according to a format.
+ * @format: list of types of arguments passed to the function.
+ *
+ * Return: the number of characters printed excluding the null byte.
  */
-int (*check_format(const char *format))(va_list)
-{
-	int i = 0;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"b", print_b},
-		{"u", print_u},
-		{"o", print_o},
-		{"x", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
 
-	for (; p[i].t != NULL; i++)
-	{
-		if (*(p[i].t) == *format)
-			break;
-	}
-	return (p[i].f);
-}
-
-/**
- * _printf - function for format printing
- * @format: list of arguments to printing
- * Return: Number of Characters to printing
- */
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int (*f)(va_list);
-	unsigned int i = 0, counter = 0;
+	int (*link)(va_list);
+	unsigned int i = 0, count = 0;
 
 	if (format == NULL)
 		return (-1);
@@ -50,34 +20,33 @@ int _printf(const char *format, ...)
 	va_start(ap, format);
 	while (format && format[i])
 	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			counter++;
-			i++;
-			continue;
-		}
-		else
+		if (format[i] == '%')
 		{
 			if (format[i + 1] == '%')
 			{
-				_putchar('%');
-				counter++;
-				i += 2;
-				continue;
+				_putchar(37);
+				count++;
+				i = i + 1;
 			}
 			else
 			{
-				f = check_format(&format[i + 1]);
-				if (f == NULL)
+				linkn= format_checker(&format[i + 1]);
+				if (link != NULL)
+				{
+					i = i + 1;
+					count += link(ap);
+				}
+				else
 					return (-1);
-				i += 2;
-				counter += f(ap);
-				continue;
 			}
+		}
+		else
+		{
+			_putchar (format[i]);
+			count++;
 		}
 		i++;
 	}
 	va_end(ap);
-	return (counter);
+	return (count);
 }
